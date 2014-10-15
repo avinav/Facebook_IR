46ddafcd68e1b2cd0af0819f271744d9395669ff
avinav <sharan.avinav@gmail.com>
2014-10-15 04:25:26 -0400

    execute script to retrieve doc elements

--- GetData/test1.py
+++ GetData/test1.py
@@ -19,54 +19,17 @@ ePass.send_keys(password);
 #eSearch = WebDriverWait(driver, 10).until(
 #	EC.presence_of_element_located(By.NAME("q"))
 #	)
-wait = WebDriverWait(driver, 10);
-eSearch = wait.until(EC.visibility_of_element_located((By.NAME,'q')))
 
-
-
+#eSearch = wait.until(EC.visibility_of_element_located((By.NAME,'q')))
+eSearch = driver.execute_script("return document.getElementsByName('q')[0]")
 #eSearch = driver.find.element_by_id("u_0_d");
-#eSearch.send_keys("people at university at buffalo");
+print eSearch
 
-
-js = """document.getElementsByName('q')[0].focus();
-	document.getElementsByName('q')[0].value='people at university at buffalo';
-	var ev = document.createEvent('KeyboardEvent');
-	ev.initKeyEvent( 'keydown', true, true, window, false, false, false, false, 13, 0); 
-	document.body.dispatchEvent(ev);""";
-js1 = """document.getElementsByName('q')[0].focus();
-document.getElementsByName('q')[0].value='People who go to University at Buffalo Graduate School ';
-Podium = {};
-Podium.keydown = function(k) {
-    var oEvent = document.createEvent('KeyboardEvent');
-
-    // Chromium Hack
-    Object.defineProperty(oEvent, 'keyCode', {
-                get : function() {
-                    return this.keyCodeVal;
-                }
-    });     
-    Object.defineProperty(oEvent, 'which', {
-                get : function() {
-                    return this.keyCodeVal;
-                }
-    });     
-
-    if (oEvent.initKeyboardEvent) {
-        oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, k, k);
-    } else {
-        oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, k, 0);
-    }
-
-    oEvent.keyCodeVal = k;
-
-    if (oEvent.keyCode !== k) {
-        alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
-    }
-
-    document.dispatchEvent(oEvent);
-}
-Podium.keydown(13);"""
-driver.execute_script(js1);
+eSearch.send_keys("people at university at buffalo\n");
+driver.implicitly_wait(10)
+list = driver.execute_script("return document.getElementById('browse_result_area')")
+print list
+#driver.execute_script("return document.getElementsByName('q')[0]");
 
 #eButton = driver.find_element_by_class_name('_585_');
 #eButton.click();
